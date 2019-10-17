6305d1e0aeb01bce2d51c96999a199bbf9dc6f09
thinklim <sagalim3983@gmail.com>
2019-10-17 16:12:58 +0900

    addIntentModel
    first

--- waste/views.py
+++ waste/views.py
@@ -2,11 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import City
 import json
+import pickle
+from waste.intent import predictions, get_final_output
 
 
 def post(request):
     if request.method == "POST":
         # get_value = request.body
+
+        # Load pickle
+        with open("data/unique_intent.pickle", "rb") as fr:
+            unique_intent = pickle.load(fr)
+
+        text = "방법"
+        pred = predictions(text)
+        get_final_output(pred, unique_intent)
 
         print(request.body)
 
